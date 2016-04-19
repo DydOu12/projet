5a4f15b275c2e1254e7ceb8b23c54ef4e2d52653
dylan monneau <dylan monneau>
2016-04-19 17:24:03 +0200

    Suppression2

--- test/script.js
+++
@@ -1,6 +0,0 @@
-var a = document.getElementById("calendarMain2");
-var b = document.getElementById("champ_date2");
-
-//<![CDATA[
-	calInit(a, "Calendrier", b, "jsCalendar", "day", "selectedDay");
-//]]>
