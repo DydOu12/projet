00a8c7d65aa3afcd86ab67e58dd6d6482d14540a
dylan monneau <dylan monneau>
2016-04-26 19:56:37 +0200

    commentaire python

--- server/python/accueil.py
+++ server/python/accueil.py
@@ -17,17 +17,23 @@ def index():
 	"""
 		test
 	"""
+	# if the database already exists
 	if os.path.isfile(DATA_PATH):
-		
+		# informations from database administrators are selected
 		admins = FunctionDataBase.selectAdmin()
 		
+		# variable that count the number of administrators (number if line in database)
 		i = 0
 		
+		# for selected lines in database
 		for pers in admins:
+			# the number of administrator is incremented 
 			i = i+1
 			
+		# if there's not administrators in database
 		if(i == 0):
 			return template('accueil',erreur="",bd="",admin="True")
+		# if not, there is (are)
 		else:
 			return template('accueil',erreur="",bd="",admin="")
 			
@@ -36,228 +42,305 @@ def index():
 @post('/')
 def index():
 	
+	# informations from form are recovered (whether administrator creation, whether authentication)
 	types = request.forms.get('types')
 	
+	# if it's the administrator creation form
 	if(types == "create"):
-		
+		# the pseudo and the password 's new administrator
 		nomCreate = request.forms.get('nomCreate')
 		passwordCreate = request.forms.get('passwordCreate')
 		
+		# if the pseudo is empty
 		if(nomCreate == ""):
 			return template('accueil',erreur="Un nom est obligatoire pour l'administrateur",bd="",admin="True")
+		# if the password is empty
 		elif(passwordCreate == ""):
 			return template('accueil',erreur="Un mot de passe est obligatoire pour l'administrateur",bd="",admin="True")
+		# if it's OK
 		elif(nomCreate != "" and passwordCreate != ""):
-			
+			# the password is encrypted
 			hash_object = hashlib.sha1(passwordCreate.encode())
 			hex_digPassword = hash_object.hexdigest()
 			
-			# FunctionDataBase.CreateTableAdmin()
+			# the administrator that has been just created is inserted in database
 			FunctionDataBase.insertAdmin(nomCreate,hex_digPassword)
 
 			return template('accueil',erreur="L'administrateur a bien été créé",bd="",admin="")
-			
-		
-			
-		
+				
+	# if not, if it's the administrator authentication form	
 	else:
-	
+		# the pseudo and the password seized by the user is recovered
 		nom = request.forms.get('nom')
 		password = request.forms.get('password')
 		
+		# if the pseudo is empty
 		if(nom == ""):
 			return template('accueil',erreur="Un administrateur a un nom",bd="",admin="")
+		# if the password is empty
 		elif(password == ""):
 			return template('accueil',erreur="Mot de passe obligatoire",bd="",admin="")
+		# if it's OK
 		elif(nom != "" and password != ""):
-		
+			# the password linked to the pseudo is selected (it's a sqlite3.Cursor object)
 			bdPassword = FunctionDataBase.selectPassAdmin(nom)
 			
+			# variable that count the number of administrators (number of line in database)
 			i=0
 			
+			# the cursor sqlite3 object is traversed containing the password
 			for j in bdPassword:
+				# the number of administrator is incremented 
 				i = i+1
+				# the password in database is stored
 				passwordStocker = j[0]
 
-			
+			# if the number of administrator is equal to 0 (there is no lines in database)
 			if(i == 0):
 				return template('accueil',erreur="L'administrateur n'existe pas",bd="",admin="")
+			# if not, if there is (are) administrators in database
 			else:
-				
-				# Vérifier si le mot de passe écrit encoder corresponds à celui en base
-				
+				# the password seized is encrypted in order to compare this in database
 				hash_object = hashlib.sha1(password.encode())
 				hex_digPassword = hash_object.hexdigest()
 				
-				# Si les mots de passe de correspondent pas alors message d'erreur
-				
+				# if there are different
 				if(hex_digPassword != passwordStocker):
 					return template('accueil',erreur="Le mot de passe ou le login est erroné.",bd="",admin="")
+				# if not, if there are equals 
 				else:
-					
+					# everything is selected from Admin table in database (still a sqlite3.Cursor object)
 					admins = FunctionDataBase.selectAdmin()
 					
+					# ID linked with the pseudo seized is selected (still a sqlite3.Cursor object)
 					bdID = FunctionDataBase.selectIdAdmin(nom)
 					
+					# the cursor sqlite3 object is traversed containing the ID
 					for j in bdID:
 						idPers = j[0]
 					
 					listeAdmin = []
 					
+					# the cursor sqlite3 object is traversed containing all informations on all administrators
 					for personnes in admins:
+						# for each line in Admin table, information will be add in this array
 						personne = []
+						# all informations concerning one administrator (ID, pseudo, password) is traversed
 						for information in personnes:
+							# an array on these informations is created (so 3 parts)
 							personne.append(information)
+						# an array of array is got
 						listeAdmin.append(personne)
-							
-					
+								
 					return template('admin',cles=passwordStocker, clesVerification=nom,admin=listeAdmin,idPers=idPers,information="",creation="",generation="")
 			
 
 @get('/creation')
 def index():
+
+	# if the database already exists
 	if os.path.isfile(DATA_PATH):
 		return template('accueil',erreur="La base de donnée est déjà existante",bd="",admin="")
+	# if not, if the database does not exist
 	else:
 		listeAdmin = []
+		# all tables for the database are created
 		genererBD.test(listeAdmin)
 		return template('accueil',erreur="La base a bien été créé",bd="",admin="True")
 
 
 @get('/admin')
 def index():
+
+	# if the database already exists
 	if os.path.isfile(DATA_PATH):
 		return template('accueil',erreur="Accès refusé, merci de vous connectez pour accéder à cette partie du site",bd="",admin="")
+	# if not, if the database does not exist
 	else:
 		return template('accueil',erreur="Accès refusé, merci de vous connectez pour accéder à cette partie du site",bd="false",admin="")
 
 
 @post('/admin')
 def index():
-	
+
+	# contain the password of the administrator when he logged
 	password = request.forms.get('cles')
+	# contain the pseudo of the administrator when he logged
 	nom = request.forms.get('clesVerification')
 	
+	# ID linked with the pseudo is selected (it's a sqlite3.Cursor object)
 	idAdmin = FunctionDataBase.selectIdWithpassAdmin(nom,password)
 	
+	# variable that count the number of administrators (number of line in database)
 	i=0
 	
+	# the cursor sqlite3 object is traversed containing all informations on all administrators
 	for j in idAdmin:
 		i = i+1
 
-	
+	# if the number of administrator is equal to 0 (there is no lines in database)
 	if(i == 0):
 		return template('accueil',erreur="Merci de vous connectez pour accéder à cette partie du site",bd="",admin="")
+	# if not, if there is (are) administrators in database
 	else:
+		# the type of the form is recovered to know which action will be realised
 		types = request.forms.get('types')
 		
+		# the id of the logged administrator is recovered
 		idPers = request.forms.get('idPers')
 		
+		# the id of the logged administrator is recovered (will be usefull to remove an administrator)
 		idSelect = request.forms.get('idSelect')
 		
+		# everything is selected from the Admin table
 		admins = FunctionDataBase.selectAdmin()
 			
 		listeAdmin = []
 		
+		# the cursor sqlite3 object is traversed containing all informations on all administrators
 		for personnes in admins:
+			# for each line in Admin table, information will be add in this array
 			personne = []
+			# all informations concerning one administrator (ID, pseudo, password) is traversed
 			for information in personnes:
+				# an array on these informations is created (so 3 parts)
 				personne.append(information)
+			# an array of array is got 
 			listeAdmin.append(personne)
 		
+		# if the focus form is this to change the password of one administrator
 		if(types == "modification"):
+			# the new password seized for the administrator is recovered
 			newPassword = request.forms.get('newPassword')
 						
+			# if the new password is empty	
 			if(newPassword == ""):
 				return template('admin',cles=password, clesVerification=nom,admin=listeAdmin,idPers=idPers,information="Merci de rentrer un mot de passe",creation="",generation="")
+			# if not, if it's all right
 			else:	
 				hash_object = hashlib.sha1(newPassword.encode())
 				hex_digPassword = hash_object.hexdigest()
 						
-
+				# the new password is edited in the database
 				FunctionDataBase.updatePassAdmin(idSelect,hex_digPassword)
 
 				return template('admin',cles=hex_digPassword, clesVerification=nom,admin=listeAdmin,idPers=idPers,information="Le mot de passe de l'administrateur "+nom+" a bien été modifié",creation="",generation="")
-			
+		
+		# if the focus form is this to delete an administrator
 		elif(types == "delete"):
+			# if the administrator wanting to be deleted is the one currently logged
 			if(idSelect == idPers):
 				return template('admin',cles=password, clesVerification=nom,admin=listeAdmin,idPers=idPers,information="Vous ne pouvez pas supprimer "+nom+" car il est actuellement connecté",creation="",generation="")
+			# if not, he isn't
 			else:
+				# the pseudo of the administrator that will be remove is selected (it's a sqlite3.Cursor object)
 				nomPersDel = FunctionDataBase.selectNomAdmin(idSelect)
 				
+				# inside this object, the pseudo is recovered
 				for i in nomPersDel:
 					nomDel = i[0]
-					
+				
+				# the administrator is deleted as well
 				FunctionDataBase.delAdmin(idSelect)
 				
+				# everything is selected from the Admin table
 				admins = FunctionDataBase.selectAdmin()
 			
 				listeAdmin = []
 				
+				# the cursor sqlite3 object is traversed containing all informations on all administrators
 				for personnes in admins:
+					# for each line in Admin table, information will be add in this array
 					personne = []
+					# all informations concerning one administrator (ID, pseudo, password) is traversed
 					for information in personnes:
+						# an array on these informations is created (so 3 parts)
 						personne.append(information)
+					# an array of array is got 
 					listeAdmin.append(personne)
 				
 
 				return template('admin',cles=password, clesVerification=nom,admin=listeAdmin,idPers=idPers,information=nomDel+" a bien été supprimé",creation="",generation="")
-		elif(types == "add"):
-			
-			nomCreate = request.forms.get('nom')
 		
+		# if the focus form is this to add an administrator
+		elif(types == "add"):
+			# the pseudo of the new administrator is recovered
+			nomCreate = request.forms.get('nom')
+			# the password of the new administrator is recovered
 			passwordCreate = request.forms.get('password')
 			
+			# if the pseudo is empty
 			if(nomCreate == ""):
 				return template('admin',cles=password, clesVerification=nom,admin=listeAdmin,idPers=idPers,information="",creation="Le nom de l'administrateur est inexistant",generation="")
+			# if not, if the password is empty
 			elif(passwordCreate == ""):
 				return template('admin',cles=password, clesVerification=nom,admin=listeAdmin,idPers=idPers,information="",creation="Le mot de passe pour " + nomCreate+ " est inexistant",generation="")
+			# if not, if it's all right
 			else:
+				# the password is encrypted
 				hash_object = hashlib.sha1(passwordCreate.encode())
 				hex_digPassword = hash_object.hexdigest()
 				
+				# the ID linked with the pseudo is selected (it's a sqlite3.Cursor object)
 				nomPersCreate = FunctionDataBase.selectIdAdmin(nomCreate)
 				
+				# variable that count the number of administrators (number of line in database)
 				i=0
 	
+				# the sqlite3.Cursor object is traversed 
 				for j in nomPersCreate:
 					i = i+1
-					
+				
+				# if there's something in this object, it's that the pseudo is already used
 				if(i != 0):
 					return template('admin',cles=password, clesVerification=nom,admin=listeAdmin,idPers=idPers,information="",creation=nomCreate + " existe déjà, veuillez rentrer un autre nom",generation="")
 				
+				# the new administrator and his password are inserted into database
 				FunctionDataBase.insertAdmin(nomCreate,hex_digPassword)
 				
+				# everything is selected from the Admin table
 				admins = FunctionDataBase.selectAdmin()
 				
 				listeAdmin = []
 				
+				# the cursor sqlite3 object is traversed containing all informations on all administrators
 				for personnes in admins:
+					# for each line in Admin table, information will be add in this array
 					personne = []
+					# all informations concerning one administrator (ID, pseudo, password) is traversed
 					for information in personnes:
+						# an array on these informations is created (so 3 parts)
 						personne.append(information)
+					# an array of array is got 
 					listeAdmin.append(personne)
 					
 				return template('admin',cles=password, clesVerification=nom,admin=listeAdmin,idPers=idPers,information="",creation=nomCreate + " a bien été créé",generation="")
-				
+		
+		#if the focus form is this to regenerate the CSV
+		# for example, if the CSV is edited by removing some lines, the database could be regenerate to update the informations for users
 		elif(types == "generation"):
-			
+			# everything is selected from the Admin table
 			admins = FunctionDataBase.selectAdmin()
 				
 			listeAdmin = []
 			
+			# the cursor sqlite3 object is traversed containing all informations on all administrators
 			for personnes in admins:
+				# for each line in Admin table, information will be add in this array
 				personne = []
+				# all informations concerning one administrator (ID, pseudo, password) is traversed
 				for information in personnes:
+					# an array on these informations is created (so 3 parts)
 					personne.append(information)
+				# an array of array is got 
 				listeAdmin.append(personne)
 			
+			# the database is removed
 			os.remove(DATA_PATH)
+			# the it's regenerate
 			genererBD.test(listeAdmin)
-			#os.system("../BD/genererBD.py")
 			
 			return template('admin',cles=password, clesVerification=nom,admin=listeAdmin,idPers=idPers,information="",creation="",generation="La base a bien été mise à jour")
-
 
 
 @get('/recherche') # ==> @route('/recherche')
@@ -265,26 +348,40 @@ def index():
 	
 	liste = []
 
+	# ActLib attribute is selected from Activite table
 	activites = FunctionDataBase.selectActivite('ActLib')
 
+	# all activities are traversed 
 	for i in activites:
+		# check if the current element is in the list
 		present = i[0] in liste 
+		# if the current element isn't present in the list
 		if(present == False):
+			# it's add to the list
 			liste.append(i[0])
 	
+	# the list is sorted
 	liste.sort()
+	# the head of the column of the CSV is removed
 	liste.remove('ActLib')
 
 	entrainement = []
 
+	# ActLib attribute is selected from Activite table
 	nivact = FunctionDataBase.selectActivite('ActNivLib')
 
+	# all levels are traversed 
 	for j in nivact:
+		# check if the current element is in the list
 		present = j[0] in entrainement
+		# if the current element isn't present in the list
 		if(present == False):
+			# it's add to the list
 			entrainement.append(j[0])
 
+	# the head of the column of the CSV is removed
 	entrainement.remove('ActNivLib')
+	# the list is sorted
 	entrainement.sort()
 
 	return template('formulaire',liste=liste, nivact=entrainement)
@@ -292,6 +389,7 @@ def index():
 @post('/recherche')
 def post():
 	
+	# each information necessary are recovered
 	activite = request.forms.get('activite')
 	codePostal = request.forms.get('codePostal')
 	niveau = request.forms.get('niveau')
@@ -299,33 +397,47 @@ def post():
 
 	information = []
 
+	# informations are selected according on what the user choose
 	listeActivite = FunctionDataBase.listeActivite(activite,codePostal,niveau,handicap)
 	
+	# the cursor sqlite3 object is traversed containing all informations of the research
 	for i in listeActivite:
+		# all necessary information will be add in this array
 		valeurListe = []
+		# all informations concerning one response of the request made by the user
 		for j in i:
+			# an array on these informations is created 
 			valeurListe.append(j)
-	
+		
+		# informations linked to Equipement table are recovered (it's a cursor sqlite3 object)
 		listeEquipement = FunctionDataBase.selectEquipement(valeurListe[1])
 
+		# all informations concerning one request linked to Equipement
 		for k in listeEquipement:
+			# in this request
 			for l in k:
+				# each element are add in the list
 				valeurListe.append(l)
 
+		# if the user don't specify anything about handicap
 		if(handicap != ""):
 			listeInstallation = FunctionDataBase.selectAllInstTwoArg(valeurListe[9],handicap)
+		# if not
 		else:
 			listeInstallation = FunctionDataBase.selectAllInstOneArg(valeurListe[9])
 
-
+		# all informations concerning one request linked to Installation
 		for m in listeInstallation:
+			# in this request
 			for n in m:
+				# each element are add in the list
 				valeurListe.append(n)
 
+		# if the list length is out of 20, informations are added to the list because 
+		# all informations must be taken into account
 		if(len(valeurListe) > 20):
 			information.append(valeurListe)
 			
-	
 	return template('resultat',information = information)
 
 @get('/recherche/<id>')
@@ -333,26 +445,39 @@ def index(id):
 
 	information =[]
 
+	# everything is selected with one specific ID
 	listeActivite = FunctionDataBase.selectAllActivite(int(id))
 
+	# the cursor sqlite3 object is traversed containing all informations of the research
 	for i in listeActivite:
+		# all necessary information will be add in this array
 		valeurListe = []
+		# all informations concerning one response of the request made by the user
 		for j in i:
+			# an array on these informations is created 
 			information.append(j)
 	
+		# informations linked to Equipement table are recovered (it's a cursor sqlite3 object)
 		listeEquipement = FunctionDataBase.selectAllEquipement(information[1])
 
+		# all informations concerning one request linked to Equipement
 		for k in listeEquipement:
+			# in this request
 			for l in k:
+				# each element are add in the list
 				information.append(l)
 
-
+		# informations linked to Installation table are recovered (it's a cursor sqlite3 object)
 		listeInstallation = FunctionDataBase.selectAllInstallation(information[9])
 
+		# all informations concerning one request linked to Installation
 		for m in listeInstallation:
+			# in this request
 			for n in m:
+				# each element are add in the list
 				information.append(n)
 
 	return template('carte',information = information)
 
+# lauching of the server
 run(host='localhost', port=8080)
--- server/python/genererBD.py
+++ server/python/genererBD.py
@@ -4,8 +4,12 @@ import hashlib
 import os
 import os.path
 
+"""
+
+"""
 def test(listeAdmin):
 
+	# opening of all resources files
 	nameAct = "../../ressources/Activite.csv"
 	fileAct = open(nameAct, "r")
 
@@ -17,149 +21,132 @@ def test(listeAdmin):
 
 
 
-
-	# ACTIVITE
+	# ACTIVITY
+	"""
 	# Fonction permettant de créer la table Activité avec différents paramètre
-
+	"""
 	def CreateTableActivite():
 		conn.execute('''CREATE TABLE Activite 
 		(ID INT PRIMARY KEY,EquipementID INT, ActCode INT,ActLib TEXT,ActNivLib TEXT,ComInsee INT)''')
-		#id => EquipementID
 
+	"""
 	# Fonction permettant d'ajouter des lignes à la table activité
-
+	"""
 	def AddEntryActivite(ID,EquipementID,ActCode,ActLib,ActNivLib,ComInsee):
 		conn.execute('''INSERT INTO Activite (ID,EquipementID,ActCode,ActLib,ActNivLib,ComInsee)
 		VALUES (?,?,?,?,?,?)''',(ID,EquipementID,ActCode,ActLib,ActNivLib,ComInsee))
 
 
 
-
 	# EQUIPEMENT
+	"""
 	# Fonction permettant de créer la table Equipement avec différents paramètre
-
+	"""
 	def CreateTableEquipement():
 		conn.execute('''CREATE TABLE Equipement 
 		(ID INT PRIMARY KEY,EquipementID INT, ComLib TEXT,InsNumeroInstall TEXT,InsNom TEXT,EquNom TEXT,EquipementTypeLib TEXT,EquGPSX REAL,EquGPSY REAL)''')
-		#id => EquipementID
 
+	"""
 	# Fonction permettant d'ajouter des lignes à la table activité
-
+	"""
 	def AddEntryEquipement(ID,EquipementID,ComLib,InsNumeroInstall,InsNom,EquNom,EquipementTypeLib,EquGPSX,EquGPSY):
 		conn.execute('''INSERT INTO Equipement (ID,EquipementID,ComLib,InsNumeroInstall,InsNom,EquNom,EquipementTypeLib,EquGPSX,EquGPSY)
 		VALUES (?,?,?,?,?,?,?,?,?)''',(ID,EquipementID,ComLib,InsNumeroInstall,InsNom,EquNom,EquipementTypeLib,EquGPSX,EquGPSY))
 
 
 
-
-
-
-
-
 	# INSTALLATION
+	"""
 	# Fonction permettant de créer la table Equipement avec différents paramètre
-
+	"""
 	def CreateTableInstallation():
 		conn.execute('''CREATE TABLE Installation 
 		(ID INT PRIMARY KEY,InsNumeroInstall INT, NomVoie TEXT,AmenagementAccesHand BOOLEAN,NbrEquip INT,PlaceParking INT)''')
-		#id => EquipementID
 
+	"""
 	# Fonction permettant d'ajouter des lignes à la table activité
-
+	"""
 	def AddEntryInstallation(ID,InsNumeroInstall,NomVoie,AmenagementAccesHand,NbrEquip,PlaceParking):
 		conn.execute('''INSERT INTO Installation (ID,InsNumeroInstall,NomVoie,AmenagementAccesHand,NbrEquip,PlaceParking)
 		VALUES (?,?,?,?,?,?)''',(ID,InsNumeroInstall,NomVoie,AmenagementAccesHand,NbrEquip,PlaceParking))
 
 
 
-
-
-
 	# ADMINISTRATION
+	"""
 
+	"""
 	def CreateTableAdmin():
 		conn.execute('''CREATE TABLE Admin 
 		(ID INTEGER PRIMARY KEY AUTOINCREMENT,Nom TEXT, Password TEXT)''')
-		#id => EquipementID
-		
+	
+	"""
+	"""
 	def AddEntryAdmin(Nom,Password):
 		conn.execute('''INSERT INTO Admin (Nom,Password)
 		VALUES (?,?)''',(Nom,Password))
 
-
-
-	# Entre Equipement et Activité EquipementID
-	# Entre Equipement et installation NumeroInstallation
-
-	# Création du fichier 
+	# Connection to the database
 
 	conn = sqlite3.connect('../../Data Base/DataBase.db')
 	c = conn.cursor()
 
-	# Création de la table Activité, Equipement, installation
+	# Creation of Activity, Equipement and Installation table
 
 	CreateTableActivite()
 	CreateTableEquipement()
 	CreateTableInstallation()
 
-	# Lecture du fichier CVS Activite
-
+	# Reading of CSV Activity file 
 	try:
-		#
-		# Création du ''lecteur'' CSV.
-		#
+		# Creation of ''reader'' CSV.
 		reader = csv.reader(fileAct)
-		#
-		# Le ''lecteur'' est itérable, et peut être utilisé
-		# dans une boucle ''for'' pour extraire les
-		# lignes une par une.
-		#
+		# The ''reader'' is iterable, and can be used
+		# in a for loop to extract lines one by one
 		for i,row in enumerate(reader):
 			accent = ['é', 'è', 'ê', 'à', 'ù', 'û', 'ç', 'ô', 'î', 'ï', 'â']
 			sans_accent = ['e', 'e', 'e', 'a', 'u', 'u', 'c', 'o', 'i', 'i', 'a']
+			# the level of the competition (with accents)
 			ligne = row[9]
 			j = 0
 			while j < len(accent):
+				# all accents are replace bu letter without accent
 				ligne = ligne.replace(accent[j], sans_accent[j])
 				j += 1
+			# interesting data are added to the database
 			AddEntryActivite(i,row[2],row[4],row[5],ligne,row[0])
 		
 	finally:
-		#
-		# Fermeture du fichier source
-		#
+		# Closing of the source file
 		fileAct.close()
 
 
-	# Lecture du fichier CVS Equipement
-
+	# Reading of CSV Equipement file
 	try:
 		reader = csv.reader(fileEquip)
 		for i,row in enumerate(reader):
+			# interesting data are added to the database
 			AddEntryEquipement(i,row[4],row[1],row[2],row[3],row[5],row[7],row[180],row[181])
-			# print(row)
 	finally:
+		# Closing of the source file
 		fileEquip.close()
 
 
-	# Lecture du fichier CSV Installation
-
+	# Reading of CSV Installation file
 	try:
 		reader = csv.reader(fileInst)
 		for i,row in enumerate(reader):
+			# interesting data are added to the database
 			AddEntryInstallation(i,row[1],row[7],row[12],row[26],row[17])
-			# print(row)
 	finally:
+		# Closing of the source file
 		fileEquip.close()
 		
 	CreateTableAdmin()
 
 	conn.commit()
 
-
-
-	# On ferme toutes les actions relative aux SQL
-
+	# All actions from SQL are closed
 	conn.close()
 
 
